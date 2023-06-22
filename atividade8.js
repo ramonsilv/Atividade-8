ae6778e2442e13dc207157c47aaa5de87b7025a6
Ramon Silva Freitas <ramonsilvaf@gmail.com>
2023-06-22 17:26:19 -0300

    ativivdade adcionada do 4 ate 6

--- atividade8.js
+++ atividade8.js
@@ -1,9 +1,9 @@
 let nomes = ['joão', 'david', 'Maria',
-    'Woshinton', 'Pepe','andré', 
-    'Lucas', 'Raquel','antonio', 
-    'cleitin', 'onorio', 'Ana', 
-    'Madalena','Helena', 'Juliana',
-    'Sandra','samuel', 'david',];
+    'Woshinton', 'Pepe', 'andré',
+    'Lucas', 'Raquel', 'antonio',
+    'cleitin', 'onorio', 'Ana',
+    'Madalena', 'Helena', 'Juliana',
+    'Sandra', 'samuel', 'david',];
 let notas = ['6.1', '3.8', '0.3',
     '5.8', '2.8', '9.5',
     '4.1', '3.1', '5.7',
@@ -15,19 +15,39 @@ alunos = [];
 
 for (i = 0; i < nomes.length; i++) {
 
-    Alunos = {
-        alunos: nomes[i],
-        nota: notas[i],
-        
+    Aluno = {
+        "alunos": nomes[i],
+        "nota": notas[i],
+
     }
-    alunos.push(Alunos)
+    alunos.push(Aluno)
 }
-console.log(alunos)
+// console.log(alunos)
 
 alunos.forEach(elemento => {
     elemento["situação"] = elemento["nota"] >= 7.0 ? "Aprovado" : "Reprovado"
 });
 
-// console.log(alunos) //(EXERCICIO 3)
+// console.log(alunos) //(EXERCICIO 3 e EXERCIO 4)
 
-let mapa = new Map()
+mapaSala = new Map()
+
+for (i = 0; i < alunos.length; i++) {
+    Aluno = alunos[i]
+
+    mapaSala.set(Aluno.alunos, Aluno);
+
+}
+
+// console.log(mapaSala)
+
+// console.log(mapaSala.get("Pepe").nota);(EXERCICIO 5)
+
+chave = Array.from(mapaSala.keys())
+quartaElemento = chave[3]
+mapaSala.delete(quartoElemento)
+console.log(mapaSala)
+
+
+
+
